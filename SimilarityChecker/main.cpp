6e5a80f529b051457abd5ee71fa322b0c01fec74
Bongsu Jeon <bs.jeon87@gmail.com>
2025-03-28 11:07:51 +0900

    Add the alpha score feature

--- SimilarityChecker/main.cpp
+++ SimilarityChecker/main.cpp
@@ -1,7 +1,30 @@
 #include "gmock/gmock.h"
 #include "similarityChecker.cpp"
+#include <string>
 using namespace testing;
+using namespace std;
 
+class SimilarityCheckerFixture : public Test
+{
+public:
+	SimilarityChecker similarityChecker;
+};
+
+TEST_F(SimilarityCheckerFixture, alpha1) {
+	EXPECT_EQ(40, similarityChecker.getScore("AABC", "BCA"));
+}
+
+TEST_F(SimilarityCheckerFixture, alpha2) {
+	EXPECT_EQ(24, similarityChecker.getScore("AABCE", "KBCA"));
+}
+
+TEST_F(SimilarityCheckerFixture, alpha44) {
+	EXPECT_EQ(0, similarityChecker.getScore("A", "BB"));
+}
+
+TEST_F(SimilarityCheckerFixture, alpha3) {
+	EXPECT_EQ(24, similarityChecker.getScore("KBCA", "AABCE"));
+}
 
 int main() {
 	InitGoogleMock();
--- SimilarityChecker/similarityChecker.cpp
+++ SimilarityChecker/similarityChecker.cpp
@@ -1,4 +1,36 @@
 
-class SimilarityChecker {
+#include <string>
+#include <set>
 
+using std::string;
+using std::set;
+
+class SimilarityChecker {
+private:
+	float getAlphaScore(const string& inputA, const string& inputB) {
+		set<char> charForA;
+		set<char> charForB;
+		int sameCnt = 0;
+		int totalCnt = 0;
+
+		for (char input : inputA) {
+			charForA.insert(input);
+		}
+		for (char input : inputB) {
+			charForB.insert(input);
+		}
+
+		for (char input : charForA) {
+			if (charForB.find(input) != charForB.end()) {
+				sameCnt++;
+			}
+		}
+		totalCnt = (charForA.size() + charForB.size() - sameCnt);
+
+		return ((float)sameCnt / totalCnt) * 40;
+	}
+public:
+	float getScore(const string& inputA, const string& inputB) {
+		return getAlphaScore(inputA, inputB);
+	}
 };
