afb94f62fd9d6fa5065be4e929b27607d0381f56
Bongsu Jeon <bs.jeon87@gmail.com>
2025-03-28 09:59:36 +0900

    Add validation code

--- SimilarityChecker/main.cpp
+++ SimilarityChecker/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace testing;
 using namespace std;
+
 TEST(lengthChecker, length1)
 {
 	SimilarityChecker similarityChecker;
@@ -10,6 +11,15 @@ TEST(lengthChecker, length1)
 	string inputB = "bbbb";
 
 	EXPECT_EQ(60, similarityChecker.getLengthScore(inputA, inputB));
+}
+
+TEST(lengthChecker, length2)
+{
+	SimilarityChecker similarityChecker;
+	string inputA = "abab";
+	string inputB = "bb";
+
+	EXPECT_EQ(0, similarityChecker.getLengthScore(inputA, inputB));
 }
 
 
--- SimilarityChecker/similarityChecker.cpp
+++ SimilarityChecker/similarityChecker.cpp
@@ -7,6 +7,9 @@ class SimilarityChecker {
 public:
 	int getLengthScore(string inputA, string inputB)
 	{
+		if (inputA.length() > inputB.length())
+			return (1 - (inputA.length() - inputB.length()) / inputB.length()) * 60;
+
 		return 60;
 	}
 };
