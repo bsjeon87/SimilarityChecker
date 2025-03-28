a88936b810a701969be8112878a06a55afcd97a0
Bongsu Jeon <bs.jeon87@gmail.com>
2025-03-28 09:51:24 +0900

    Add validation code(same length)

--- SimilarityChecker/main.cpp
+++ SimilarityChecker/main.cpp
@@ -1,6 +1,16 @@
 #include "gmock/gmock.h"
 #include "similarityChecker.cpp"
+#include <string>
 using namespace testing;
+using namespace std;
+TEST(lengthChecker, length1)
+{
+	SimilarityChecker similarityChecker;
+	string inputA = "abab";
+	string inputB = "bbbb";
+
+	EXPECT_EQ(60, similarityChecker.getLengthScore(inputA, inputB));
+}
 
 
 int main() {
--- SimilarityChecker/similarityChecker.cpp
+++ SimilarityChecker/similarityChecker.cpp
@@ -1,4 +1,12 @@
 
-class SimilarityChecker {
+#include <string>
 
+using std::string;
+
+class SimilarityChecker {
+public:
+	int getLengthScore(string inputA, string inputB)
+	{
+		return 60;
+	}
 };
