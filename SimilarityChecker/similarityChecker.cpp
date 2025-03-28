0cdec5b3c077437a74bc4823e22c2a286b06482b
Bongsu Jeon <bs.jeon87@gmail.com>
2025-03-28 10:13:04 +0900

    refactoring

--- SimilarityChecker/similarityChecker.cpp
+++ SimilarityChecker/similarityChecker.cpp
@@ -10,14 +10,10 @@ public:
 		const static int MAX_LENGTH_SCORE = 60;
 		const int inputALen = inputA.length();
 		const int inputBLen = inputB.length();
+		const int longInputLength = (inputALen > inputBLen) ? inputALen : inputBLen;
+		const int shortInputLength = (inputALen < inputBLen) ? inputALen : inputBLen;
+		const int gap = longInputLength - shortInputLength;
 
-		if (inputALen > inputBLen) {
-			return (1 - (inputALen - inputBLen)) / inputBLen * MAX_LENGTH_SCORE;
-		}
-		else if (inputALen < inputBLen) {
-			return (1 - (inputBLen - inputALen)) / inputALen * MAX_LENGTH_SCORE;
-		}
-
-		return MAX_LENGTH_SCORE;
+		return (1 - gap / shortInputLength) * MAX_LENGTH_SCORE;
 	}
 };
